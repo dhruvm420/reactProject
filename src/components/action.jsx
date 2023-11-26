098aa7895a9ad5fd1f140e756a1ef961892e2fc4
Dhruv <mahajandhruv420@gmail.com>
2023-11-27 02:41:37 +0530

    fixed svgs not loading

--- src/components/action.jsx
+++ src/components/action.jsx
@@ -1,13 +1,19 @@
 import { HStack } from "@chakra-ui/react";
+import id from "../assets/id.svg";
+import toggle from "../assets/toggle.svg";
+import edit from "../assets/edit.svg";
+import del from "../assets/delete.svg";
+import approve from "../assets/approve.svg";
+import menu from "../assets/menu.svg";
 export default function Action() {
   return (
     <HStack spacing="1">
-      <img src="../../public/id.svg" alt="" width="30px" />
-      <img src="../../public/toggle.svg" alt="" width="30px" />
-      <img src="../../public/edit.svg" alt="" width="25px" />
-      <img src="../../public/delete.svg" alt="" width="25px" />
-      <img src="../../public/approve.svg" alt="" width="25px" />
-      <img src="../../public/menu.svg" alt="" width="30px" />
+      <img src={id} alt="" width="30px" />
+      <img src={toggle} alt="" width="30px" />
+      <img src={edit} alt="" width="25px" />
+      <img src={del} alt="" width="25px" />
+      <img src={approve} alt="" width="25px" />
+      <img src={menu} alt="" width="30px" />
     </HStack>
   );
 }
