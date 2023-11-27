3166d1c7c01d8133e0353925312396e9257ea2d4
Dhruv <mahajandhruv420@gmail.com>
2023-11-27 09:46:10 +0530

    dashboard colors fixed

--- src/components/mycard.jsx
+++ src/components/mycard.jsx
@@ -8,22 +8,21 @@ import {
   Text,
 } from "@chakra-ui/react";
 
-export default function MyCard( card ) {
+export default function MyCard(card) {
   return (
     <Card
       w="15vw"
       h="33vh"
       shadow="xl"
-      bg="gray.200"
-      border="1px"
-      borderColor="gray.400"
-      color="gray.400"
+      bg="gray.300"
+      color="gray.500"
       borderRadius="3xl"
       mb="8"
       mx="4"
       _hover={{
-        backgroundColor: "gray.300",
-        color: "gray.500",
+        backgroundColor: "gray.400",
+        color: "gray.600",
+        border: "2px",
         borderColor: "gray.500",
       }}
     >
@@ -34,7 +33,7 @@ export default function MyCard( card ) {
         </Heading>
       </CardHeader>
       <CardBody m="0" p="0">
-        <Text fontSize="5xl" color="gray.500" textAlign="center">
+        <Text fontSize="5xl" color="gray.600" textAlign="center">
           {card.count}
         </Text>
       </CardBody>
