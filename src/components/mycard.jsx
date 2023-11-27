8688d50eb97a679811c4e479f59071ca58a4bffa
Dhruv <mahajandhruv420@gmail.com>
2023-11-27 13:04:21 +0530

    minor fixes

--- src/components/mycard.jsx
+++ src/components/mycard.jsx
@@ -11,7 +11,7 @@ import { Link } from "react-router-dom";
 export default function MyCard({ title, count, link }) {
   return (
     <Card
-      w="16vw"
+      w="17vw"
       h="35vh"
       shadow="xl"
       bg="white"
@@ -19,22 +19,24 @@ export default function MyCard({ title, count, link }) {
       borderRadius="3xl"
       mb="8"
       mx="4"
-      px="8"
+      px="9"
     >
       <CardHeader m="0" pb="2">
-        <Text fontSize="5xl" color="gray.600" textAlign="left">
+        <Text fontSize="5xl" color="gray.600" textAlign="center">
           {count}
         </Text>
       </CardHeader>
       <CardBody m="0" p="0">
-        <Heading size="lg" textAlign="left">
+        <Heading size="lg" textAlign="center">
           {" "}
           {title}{" "}
         </Heading>
       </CardBody>
-      <CardFooter p="4" mt="2">
+      <CardFooter>
         <Link to={link}>
-          <Button colorScheme="gray">View More &rarr;</Button>
+          <Button colorScheme="gray" ml="2">
+            View More &rarr;
+          </Button>
         </Link>
       </CardFooter>
     </Card>
