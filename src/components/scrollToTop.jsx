e3a9e1b64b959f32d4f25cbe32b28b0609f6a5b9
Dhruv <mahajandhruv420@gmail.com>
2023-12-03 09:22:21 +0530

    fixed scroll pos on route change

---
+++ src/components/scrollToTop.jsx
@@ -0,0 +1,13 @@
+// ScrollToTop.jsx
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scrolls to top when pathname changes
+  }, [pathname]);
+
+  return null;
+}
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,8 +1,9 @@
 import { Box, Flex } from "@chakra-ui/react";
 import Header from "../components/header";
 import Sidebar from "../components/sidebar";
-
+import ScrollToTop from "../components/scrollToTop";
 export default function Root({ children, title }) {
+  ScrollToTop();
   return (
     <Box bg="#f7fafc">
       <Header title={title} />
