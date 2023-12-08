b723df5c435ee8b7109bab7d4171bd8a00b83b2d
Dhruv <mahajandhruv420@gmail.com>
2023-12-08 14:04:13 +0530

    slider fixed

--- src/routes/Forms/createSlider.jsx
+++ src/routes/Forms/createSlider.jsx
@@ -44,66 +44,72 @@ export default function SliderForm({ edit = false, modifyId = null }) {
     console.log(formData);
   };
   return (
-    <Root title="Slider Image">
-      <Box mx="auto" shadow="2xl" h="40vh" bg="white" borderRadius="3xl" my="4">
-        <form onSubmit={handleSubmit}>
-          <Stack p="6" spacing="4" w="55vw">
-            <HStack spacing="20">
-              <FormControl>
-                <FormLabel>Image *</FormLabel>
-                <Input
-                  type="file"
-                  onChange={handleFileChange}
-                  accept="image/*"
-                  border="none"
-                  required
-                />
-              </FormControl>
-              <FormControl>
-                <FormLabel>Position *</FormLabel>
-                <Input
-                  type="text"
-                  name="position"
-                  value={formData.position}
-                  border="1px"
-                  borderColor="blue.500"
-                  onChange={handleInputChange}
-                  required
-                />
-              </FormControl>
-            </HStack>
-            <HStack spacing="20">
-              <FormControl>
-                <FormLabel>Title *</FormLabel>
-                <Input
-                  type="title"
-                  name="title"
-                  border="1px"
-                  borderColor="blue.500"
-                  value={formData.title}
-                  onChange={handleInputChange}
-                  required
-                />
-              </FormControl>
+    <Box
+      mx="auto"
+      shadow={edit ? "0" : "2xl"}
+      h="40vh"
+      bg="white"
+      w="90vh"
+      borderRadius="3xl"
+      my="4"
+    >
+      <form onSubmit={handleSubmit}>
+        <Stack p="6" spacing="4" w="40vw" mx="auto">
+          <HStack spacing="20">
+            <FormControl>
+              <FormLabel>Image *</FormLabel>
+              <Input
+                type="file"
+                onChange={handleFileChange}
+                accept="image/*"
+                border="none"
+                required
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Position *</FormLabel>
+              <Input
+                type="text"
+                name="position"
+                value={formData.position}
+                border="1px"
+                borderColor="blue.500"
+                onChange={handleInputChange}
+                required
+              />
+            </FormControl>
+          </HStack>
+          <HStack spacing="20">
+            <FormControl>
+              <FormLabel>Title *</FormLabel>
+              <Input
+                type="title"
+                name="title"
+                border="1px"
+                borderColor="blue.500"
+                value={formData.title}
+                onChange={handleInputChange}
+                required
+              />
+            </FormControl>
 
-              <FormControl>
-                <FormLabel>Description *</FormLabel>
-                <Input
-                  type="text"
-                  name="description"
-                  border="1px"
-                  borderColor="blue.500"
-                  value={formData.description}
-                  onChange={handleInputChange}
-                />
-              </FormControl>
-            </HStack>
-            <Button type="submit" mt={4} colorScheme="blue" w="8vw" mx="auto">
-              Submit
-            </Button>
-          </Stack>
-        </form>
-      </Box>
-    </Root>
+            <FormControl>
+              <FormLabel>Description *</FormLabel>
+              <Input
+                type="text"
+                name="description"
+                border="1px"
+                borderColor="blue.500"
+                value={formData.description}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+          </HStack>
+          <Button type="submit" mt={4} colorScheme="blue" w="8vw" mx="auto">
+            Submit
+          </Button>
+        </Stack>
+      </form>
+    </Box>
   );
 }
